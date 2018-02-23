12e723e0731f1d0b6ff53d1b2d586d064f1e205c
JHurricane96 <arun.jhurricane@gmail.com>
2018-02-23 15:35:27 +0530

    Correct matrix multiplication
    - Add eager evaluation through .eval() method, eager eval stores
      result in a temp variable
    - Matrix multiplication is always eager now

--- fastmatrix/fastmatrix.hpp
+++ fastmatrix/fastmatrix.hpp
@@ -8,12 +8,12 @@ public:
   E const &get_const_derived() const { return static_cast<E const &>(*this); }
 
   T operator()(std::size_t i, std::size_t j) const {
-    get_const_derived()(i, j);
+    return get_const_derived()(i, j);
   }
 
-  std::size_t num_rows() const { get_const_derived().num_rows(); }
+  std::size_t num_rows() const { return get_const_derived().num_rows(); }
 
-  std::size_t num_cols() const { get_const_derived().num_cols(); }
+  std::size_t num_cols() const { return get_const_derived().num_cols(); }
 };
 
 template <typename T> class matrix : public expression<matrix<T>, T> {
@@ -23,32 +23,39 @@ private:
   std::size_t n_cols;
 
 public:
+  matrix() {}
+
   matrix(std::size_t n_rows, std::size_t n_cols)
       : container(n_rows * n_cols), n_rows(n_rows), n_cols(n_cols) {}
 
   matrix(std::size_t n_rows, std::size_t n_cols, T fill)
       : container(n_rows * n_cols, fill), n_rows(n_rows), n_cols(n_cols) {}
 
+  matrix(matrix<T> &&other)
+      : container(std::move(other.container)), n_rows(other.num_rows()),
+        n_cols(other.num_cols()) {}
+
   template <typename E>
   matrix(expression<E, T> const &other)
       : n_rows(other.num_rows()), n_cols(other.num_cols()),
         container(other.num_rows() * other.num_cols()) {
-    for (std::size_t i = 0; i < n_rows; ++i) {
-      for (std::size_t j = 0; j < n_cols; ++j) {
-        container[i * n_rows + j] = other(i, j);
-      }
-    }
+    assign(other.get_const_derived());
   }
 
-  template <typename E>
-  matrix& operator=(expression<E, T> const& other) {
+  template <typename E> matrix &operator=(expression<E, T> const &other) {
     assert(n_rows >= other.num_rows());
     assert(n_cols >= other.num_cols());
-    for (std::size_t i = 0; i < other.num_rows(); ++i) {
-      for (std::size_t j = 0; j < other.num_cols(); ++j) {
-        container[i * n_rows + j] = other(i, j);
-      }
+    assign(other.get_const_derived());
+    return *this;
+  }
+
+  matrix &operator=(matrix<T> &&other) {
+    if (this != &other) {
+      container = std::move(other.container);
+      n_rows = other.n_rows;
+      n_cols = other.n_cols;
     }
+    return *this;
   }
 
   T operator()(std::size_t i, std::size_t j) const {
@@ -62,27 +69,78 @@ public:
   std::size_t num_rows() const { return n_rows; }
 
   std::size_t num_cols() const { return n_cols; }
+
+  const matrix<T> &eval() const { return *this; }
+
+  void set_elt(std::size_t i, std::size_t j, T value) {
+    container[i * n_rows + j] = value;
+  }
+
+  template <typename E, typename ET>
+  void assign(expression<E, ET> const &expr) {
+    for (std::size_t i = 0; i < n_rows; ++i) {
+      for (std::size_t j = 0; j < n_cols; ++j) {
+        container[i * n_rows + j] = expr(i, j);
+      }
+    }
+  }
 };
 
 template <class E1, class E2, typename T, class Op>
-class matrix_binary_operation
-    : public expression<matrix_binary_operation<E1, E2, T, Op>, T> {
+class cwise_matrix_binary_operation
+    : public expression<cwise_matrix_binary_operation<E1, E2, T, Op>, T> {
 private:
   E1 const &expr1;
   E2 const &expr2;
 
 public:
-  matrix_binary_operation(expression<E1, T> const &expr1,
-                          expression<E2, T> const &expr2)
+  cwise_matrix_binary_operation(expression<E1, T> const &expr1,
+                                expression<E2, T> const &expr2)
       : expr1(expr1.get_const_derived()), expr2(expr2.get_const_derived()) {}
 
   T operator()(std::size_t i, std::size_t j) const {
     return Op::apply(expr1, expr2, i, j);
   }
 
+  const matrix<T> eval() const {
+    matrix<T> temp(num_rows(), num_cols());
+    temp.assign((*this));
+    return temp;
+  }
+
   std::size_t num_rows() const { return expr1.num_rows(); }
 
   std::size_t num_cols() const { return expr1.num_cols(); }
+};
+
+template <typename E1, typename E2, typename T>
+class matrix_product : public expression<matrix_product<E1, E2, T>, T> {
+private:
+  E1 const &expr1;
+  E2 const &expr2;
+  matrix<T> temp;
+
+public:
+  matrix_product(expression<E1, T> const &expr1, expression<E2, T> const &expr2)
+      : expr1(expr1.get_const_derived()), expr2(expr2.get_const_derived()),
+        temp(expr1.num_rows(), expr2.num_cols()) {
+    for (std::size_t i = 0; i < num_rows(); ++i) {
+      for (std::size_t j = 0; j < num_cols(); ++j) {
+        temp.set_elt(i, j, expr1(i, 0) * expr2(0, j));
+        for (std::size_t k = 1; k < expr1.num_cols(); ++k) {
+          temp.set_elt(i, j, expr1(i, k) * expr2(k, j) + temp(i, j));
+        }
+      }
+    }
+  }
+
+  T operator()(std::size_t i, std::size_t j) const { return temp(i, j); }
+
+  matrix<T> const &eval() const { return temp; }
+
+  std::size_t num_rows() const { return expr1.num_rows(); }
+
+  std::size_t num_cols() const { return expr2.num_cols(); }
 };
 
 template <class E1, class E2, typename T> struct matrix_add {
@@ -93,30 +151,18 @@ template <class E1, class E2, typename T> struct matrix_add {
 };
 
 template <class E1, class E2, typename T>
-matrix_binary_operation<E1, E2, T, matrix_add<E1, E2, T>>
+cwise_matrix_binary_operation<E1, E2, T, matrix_add<E1, E2, T>>
 operator+(expression<E1, T> const &expr1, expression<E2, T> const &expr2) {
   assert(expr1.num_rows() == expr2.num_rows());
   assert(expr1.num_cols() == expr2.num_cols());
-  return matrix_binary_operation<E1, E2, T, matrix_add<E1, E2, T>>(expr1,
-                                                                   expr2);
+  return cwise_matrix_binary_operation<E1, E2, T, matrix_add<E1, E2, T>>(expr1,
+                                                                         expr2);
 }
 
-template <class E1, class E2, typename T> struct matrix_multiply {
-  static T apply(expression<E1, T> const &expr1, expression<E2, T> const &expr2,
-                 std::size_t i, std::size_t j) {
-    T ans = expr1(i, 0) * expr2(0, j);
-    for (int k = 1; k < expr1.num_cols(); ++k) {
-      ans = ans + expr1(i, k) * expr2(k, j);
-    }
-    return ans;
-  }
-};
-
 template <class E1, class E2, typename T>
-matrix_binary_operation<E1, E2, T, matrix_multiply<E1, E2, T>>
-operator*(expression<E1, T> const &expr1, expression<E2, T> const &expr2) {
+matrix_product<E1, E2, T> operator*(expression<E1, T> const &expr1,
+                                    expression<E2, T> const &expr2) {
   assert(expr1.num_cols() == expr2.num_rows());
-  return matrix_binary_operation<E1, E2, T, matrix_multiply<E1, E2, T>>(expr1,
-                                                                        expr2);
+  return matrix_product<E1, E2, T>(expr1, expr2);
 }
 }
