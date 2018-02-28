74eaeed11177eaec33dedbf5e93c341ec7dd32ee
JHurricane96 <arun.jhurricane@gmail.com>
2018-03-01 02:27:15 +0530

    Add more benchmarks

--- bench/bench.cpp
+++ bench/bench.cpp
@@ -11,7 +11,7 @@ const int TRIES = 1;
 const int REPEAT = 100;
 
 template <typename T>
-matrix<T> make_matrix(size_t num_rows, size_t num_cols) {
+matrix<T> make_rand_matrix(size_t num_rows, size_t num_cols) {
   static_assert(is_floating_point<T>::value);
   static std::mt19937 gen{std::random_device{}()};
   static std::uniform_real_distribution dis(numeric_limits<T>::min(), numeric_limits<T>::max());
@@ -31,20 +31,28 @@ void print_results(string test_name, double time_s, double flops) {
 }
 
 int main() {
-  size_t row = 1E3, col = 1E3;
-  BenchTimer timer;
-  timer.reset();
-  timer.start();
-  auto a = make_matrix<float>(row, col);
-  auto b = make_matrix<float>(row, col);
-  auto c = make_matrix<float>(row, col);
-  auto d = make_matrix<float>(row, col);
+  size_t row = 1E2, col = 1E2;
+  auto a = make_rand_matrix<float>(row, col);
+  auto b = make_rand_matrix<float>(row, col);
+  auto c = make_rand_matrix<float>(row, col);
+  auto d = make_rand_matrix<float>(row, col);
   auto result = matrix<float>(row, col, 0);
-  timer.stop();
-  std::cout << timer.value() << std::endl;
+
+  BenchTimer timer;
+
   BENCH(timer, TRIES, REPEAT, result = (((a + b).eval() + c).eval() + d));
-  print_results("Eager Add", timer.value(), (double(row * col * REPEAT * 3) / timer.value()));
+  print_results("a+b+c+d (Eager)", timer.value(), (double(row * col * REPEAT * 3) / timer.value()));
+
   BENCH(timer, TRIES, REPEAT, result = a + b + c + d);
-  print_results("Lazy Add", timer.value(), (double(row * col * REPEAT * 3) / timer.value()));
+  print_results("a+b+c+d (Lazy)", timer.value(), (double(row * col * REPEAT * 3) / timer.value()));
+
+  BENCH(timer, TRIES, REPEAT, result = a * b * c * d);
+  print_results("a*b*c*d", timer.value(), (double(row * col * col * REPEAT * 3) / timer.value()));
+
+  BENCH(timer, TRIES, REPEAT, result = a * 5 + b);
+  print_results("(5*a)+b", timer.value(), (double(row * col * REPEAT * 2) / timer.value()));
+
+  BENCH(timer, TRIES, REPEAT, result = result + b * 5 - 10 - a * result;);
+  print_results("(5*a)+b", timer.value(), (double(row * col * (col + 4) * REPEAT) / timer.value()));
   return 0;
 }
